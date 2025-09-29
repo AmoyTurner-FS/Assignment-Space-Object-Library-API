c09c64cad1fcf474edbc6c2b7c51eb4ef1c2a773
AmoyTurner-FS <AKTurner1@fullsail.com>
2025-09-28 21:46:07 -0400

    week 4

--- routers/galaxys.js
+++ routers/galaxys.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const galaxyCtrl = require("../controllers/galaxys.js");
 const { upload } = require("../index.js");
+const galaxyCtrl = require("../controllers/galaxys.js");
 
 router.get("/", galaxyCtrl.index);
-router.post("/", upload.single("image"), galaxyCtrl.create);
+router.get("/new", (req, res) =>
+  res.render("galaxies/create.twig", { galaxy: null })
+);
 router.get("/:id", galaxyCtrl.show);
+router.get("/:id/edit", async (req, res) => {
+  const { Galaxy } = require("../models");
+  const g = await Galaxy.findByPk(req.params.id);
+  res.render("galaxies/edit.twig", { galaxy: g });
+});
+router.post("/", upload.single("image"), galaxyCtrl.create);
 router.put("/:id", upload.single("image"), galaxyCtrl.update);
 router.delete("/:id", galaxyCtrl.remove);
 
